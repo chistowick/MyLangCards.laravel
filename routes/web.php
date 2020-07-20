18d0239dae80bbaebc9ab6168d0f009388785275
Anatoly <chistowick@yandex.ru>
2020-07-20 16:17:12 +0300

    Из архивного проекта скопировал маршруты к AjaxController

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Маршрут к стартовой странице
+Route::get('/', function(){
+    return view('layouts.main');
 });
 
+// Маршрут для получения списка всех доступных пользователю карточек
+Route::post('ajax/get-cards', 'AjaxController@getCards');
+
+// Маршрут для обработки запроса на добавление новой карточки
+Route::post('ajax/add-card', 'AjaxController@addCard');
+
+// Маршрут для обработки запроса на редактирование карточки
+Route::post('ajax/edit-card', 'AjaxController@editCard');
+
+// Маршрут для обработки запроса на удаление карточки
+Route::post('ajax/delete-card', 'AjaxController@deleteCard');
+
+// Маршрут для обработки запроса на перемещение карточки
+Route::post('ajax/move-card', 'AjaxController@moveCard');
+
+// Группа маршрутов для аутентификации
 Auth::routes();
 
+// Маршрут, который загружается HomeController'ом, в тот момент когда пользователь авторизовался
 Route::get('/home', 'HomeController@index')->name('home');
