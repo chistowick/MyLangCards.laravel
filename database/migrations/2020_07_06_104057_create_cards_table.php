5c5034ed0c03854862475e88e437c71abb13d886
Anatoly <chistowick@yandex.ru>
2020-07-21 19:49:03 +0300

    Увеличил допустимый размер текстовых полей для слов и комментариев

--- database/migrations/2020_07_06_104057_create_cards_table.php
+++ database/migrations/2020_07_06_104057_create_cards_table.php
@@ -15,10 +15,10 @@ class CreateCardsTable extends Migration
     {
         Schema::create('cards', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('original', 20);
-            $table->string('originalComment', 50)->nullable()->default(NULL);
-            $table->string('translation', 20);
-            $table->string('translationComment', 50)->nullable()->default(Null);
+            $table->string('original', 30);
+            $table->string('originalComment', 80)->nullable()->default(NULL);
+            $table->string('translation', 30);
+            $table->string('translationComment', 80)->nullable()->default(Null);
             $table->tinyInteger('stack');
             $table->string('login', 20)->default('chistowick'); // TODO убрать логин по умолчанию
             $table->timestamps();
